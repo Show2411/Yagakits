6cdaea5f0fad889ddcf347f8298783a39ecfe6ef
Show2411 <olayinkaahmad1969@gmail.com>
2024-10-22 17:09:53 +0100

    Update style.js

--- style.js
+++ style.js
@@ -22,53 +22,26 @@ const navLinks = document.querySelectorAll('nav #UI').forEach(link => {
   })
   window.addEventListener('scroll',function(){
     var scroll = document.querySelector('.navbar1');
-    scroll.classList.toggle("reduce", window.scrollY > 60)
+    scroll.classList.toggle("reduce", window.scrollY )
   })
   window.addEventListener('scroll',function(){
     var scroll = document.querySelector('.navbar1 h1');
-    scroll.classList.toggle("reduce", window.scrollY > 60)
-  })
-  window.addEventListener('scroll',function(){
-    var scroll = document.querySelector('.backtop');
-    scroll.classList.toggle("bg-transparent", window.scrollY > 100)
+    scroll.classList.toggle("reduce", window.scrollY)
   })
 
-  function openNav(){
+
+    function openNav(){
     document.getElementById("myNav").classList.toggle("open");
     }
 
-    var mice = document.querySelector('.more');
-    mice.style.transition = "1s";
-    mice.addEventListener("mouseenter", function(){
-      mice.innerHTML = "<b>Shop</b> for more <i class='fa fa-shopping-cart'></i>";
-      var micey = document.querySelector('.more i');
-    micey.style.transitiondelay = "5s";
-    micey.style.transform = "translateX(7px)";
-
-    });
-      mice.addEventListener("mouseleave", function(){
-        mice.innerHTML = "<b>Shop</b> for more";
-    });
-    
    
 
 const activepage = window.location.pathname;
-const navlinks = document.querySelectorAll('.navbar #UD').forEach(link => {
+const navlinks = document.querySelectorAll('.sidebar #UD').forEach(link => {
   if(link.href.includes(`${activepage}`)){
     link.classList.add('active');
   }
   })
-
-
-  const Page = window.location.pathname;
-console.log(Page);
-const navs = document.querySelectorAll('.BOW #BET').forEach(link => {
-  if(link.href.includes(`${Page}`)){
-    link.classList.add('active');
-  }
-  })
-
-
 
 // SIDNAV
 
